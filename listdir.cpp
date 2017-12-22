a757bd9d037d5da490f4542a83ef2e939dffcd41
rozhsham <sarasti2400@gmail.com>
2017-12-22 13:22:28 +0900

    zadanie1.2

--- listdir.cpp
+++ listdir.cpp
@@ -1,3 +1,53 @@
+#include <windows.h>
+#include <stdio.h>
+#pragma warning( disable : 4996)
+
+static int count;
+static long long size;
+
+void dfs() {
+	// начинает перебор файлов и папок в текущей папке
+	// 1) папки . и .. пропускаем
+	// 2) если папка, то заходим в нее и вызываем dfs
+	// 3) если файл, то посчитаем его в суммах count и size
+	// 4) выходим в родительскую папку
+
+	HANDLE hFind;                   	// номер поиска
+	WIN32_FIND_DATA res;            	// результат поиска
+
+	hFind = FindFirstFile(L"*", &res);   // найти первый файл
+
+	do {
+		count++; // некоторые файлы не считаются??
+		wprintf(L"file #%d is <%s>\n", count, res.cFileName);
+
+		// if (...) { // если это подпапка
+		// 	здесь будет обход в глубину
+		// }
+		// else {// это файл
+		// size+=res....
+		// }
+	} while (FindNextFile(hFind, &res) != 0);
+	FindClose(hFind);
+	SetCurrentDirectory(L"..");
+}
+
+int main(int argc, wchar_t* argv[]) {
+	wchar_t s[512];               	// текущая папка
+	GetCurrentDirectory(512, s);  	// получить текущую папку
+	wprintf(L"Starting in: %s\n", s);
+
+	count = 0;                  	// обнулить счетчик файлов
+	size = 0;                   	// обнулить суммарный размер файлов
+	dfs();                      	// запустить обход в глубину из текущей папки
+
+	printf("File count = %d, size = %lld\n", count, size);
+	return 0;
+}
+
+//ololo
+
+/*
 #include "windows.h"
 #include "stdio.h"
 #pragma warning( disable : 4996)
@@ -29,7 +79,7 @@ int main() {
     	return 0;
 }
 
-/*void dfs() {
+void dfs() {
 	HANDLE hFind;                   	// номер поиска
 	WIN32_FIND_DATA res;            	// результат поиска
 
@@ -45,4 +95,5 @@ int main() {
 	} while (FindNextFile(hFind, &res) != 0);
 	FindClose(hFind);
 }
+
 */
